b4cd3494c0238b343d11c4a9ffbac3c81dc878a7
pushpakdemo <93195148+pushpakdemo@users.noreply.github.com>
2021-10-29 17:22:40 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,85 @@
+<?php 
+include './cpanel0.php';
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <?php include "common/head.php" ?>
+    </head>
+    <body>
+        
+       <?php include "common/upper-block.php" ?>
+
+
+                                  <div class="media-body">    
+                                        <p>
+
+                                  The office of the<b> State Selection Board</b>, Department of Higher Education was set up by Government of Odisha as a statutory body in the year 1975 under   erstwhile Education & Youth Services Department for carrying out selection of suitable candidates against the sanctioned Direct Payment posts in the state Cadre (both in teaching and non-teaching) in   the aided non-government colleges of the state. It was also entrusted with the responsibilities for selection of suitable candidates for teaching and non teaching    posts both   in the Government and aided Schools of the State in pursuance of Sub-Section(2) and (3) of Section 10 of the Odisha Education Act, 1969 (Odisha Act 15 of 1969) read with Rule 4 of the Odisha Educational (Recruitment and conditions of service of Teachers and Members of the Staff of the Aided Educational Institutions ) Rules , 1974. It was also declared by the Government of Odisha as a permanent body in the year 1987.                                        </p>
+
+                                        <p>
+                                                                        The state Government has constituted the State Selection Board with headquarters at Bhubaneswar and has conferred with the status of Heads of Department from the year 1987.   
+
+                                        </p>
+                                        <p>
+
+After the division of erstwhile Education & Youth Services Department into two separate departments in the name of Higher Education Department and the School and Mass Education Department, the <b>State Selection Board</b> has been functioning under the Department of Higher Education since 1993.  The <b>State Selection Board</b> is headed by the President and functioning with one Vice-president, two Members and a Secretary & Convener in addition   to others employees. They all are appointed by the Government and are governed by Rules, regulating their recruitment and conditions of service.
+                                        </p>
+                                        
+                                         <p>
+
+The <b>State Selection Board</b> is accountable to the Department of Higher Education of the State Government in administrative and financial matters. Secretary is the Head of the Office; and assisting him are the Assistant Secretaries, Tabulators   , Section Officer, Accountant etc. President is vested with the authorities to dispose of matters pertaining to discharge of the Board’s mandate and its smooth functioning .The <b>State Selection Board</b> has been active in selecting candidate against vacant posts of aided colleges of the state till 1992.                                        </p>
+                                      </div>
+
+
+
+                                      
+                                            <div class="media service-box wow fadeInRight">
+                                                <div class="pull-left">
+                                                    <i class="fa  fa-flag"></i>                                                    
+                                                </div>
+                                                <div class="media-body">
+                                                    <h4 class="media-heading">VISION</h4>
+                                                    <p>The State Selection Board has been established by the state Government with a vision to maintain the educational   standard   of the Non-Government   Aided Educational Institutions of the  state which have been contributing  significantly  to the education of common people in a major part of the state including remote and educationally and economically backward areas for a long time, through a process of monitoring and selecting  quality personnel  for  them.</p>
+                                                </div>
+                                            </div>
+
+                                            <div class="media service-box wow fadeInRight">
+                                                <div class="pull-left">
+                                                    <i class="fa fa-bar-chart-o"></i>
+                                                </div>
+                                                <div class="media-body">
+                                                    <h4 class="media-heading">MISSION</h4>
+                                                    <p>The mission of the State Selection Board, Odisha   is to lay down a comprehensive and  full proof procedure for recruitment of teaching and non-teaching personnel for non-government aided educational institutions of the state on a regular basis so as to maintain a uniform and healthy educational atmosphere in the state through  continuous brain storming deliberations and discussions in the    meetings of  the board. </p>
+                                                </div>
+                                            </div>
+
+                                            <div class="media service-box wow fadeInRight">
+                                                <div class="pull-left">
+                                                    <i class="fa fa-list-ul"></i>
+                                                </div>
+                                                <div class="media-body">                                
+                                                    <p>
+ The National Policy on Education formulated in 1986 and modified in 1992 aims to provide education of a comparable quality up to a given level to all students irrespective of their caste, creed, residence or sex. It aims at promotion of a nationality, a sense of common citizenship, composite culture and national integration. It lays stress on the need for a radical transformation of the education system to improve its quality at all stages and gives much greater attention to Science and Technology.The State Selection Board, Odisha endorses those national aims and objectives and pursues to acheive them.
+                                                    </p>
+                                                    
+                                                </div>
+                                                <div>
+                                                <div>
+                                                
+                                                </div></div>
+                                            </div>
+
+                                 
+
+
+ <?php include "common/lower-block.php" ?>
+
+
+
+
+
+    </body>
+</html>
